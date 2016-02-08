4095aff676a2c494734915b06414f4724b2422a3
Kier Davis <kierdavis@gmail.com>
2016-02-08 20:30:13 +0000

    Make build script a bit more flexible, and add a couple more options to the calls to pdflatex
    -file-line-error was added to make error message formatting more readable, while -halt-on-error was added to avoid having to manually type the exit command into pdflatex whenever a compilation error occurs.

--- make.sh
+++ make.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-mkdir -p build
-pdflatex -output-directory build -draftmode cv.tex || exit 1
-pdflatex -output-directory build cv.tex || exit 1
+
+build_dir="build"
+pdflatex_flags="-output-directory $build_dir -file-line-error -halt-on-error"
+
+mkdir -p $build_dir
+pdflatex $pdflatex_flags -draftmode cv.tex || exit 1
+pdflatex $pdflatex_flags cv.tex || exit 1
